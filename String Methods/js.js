ebae997345783d4acd50c122857c0aad39b5411c
Developertaposroy <taposroy616@gmail.com>
2023-03-22 14:39:42 +0600

    String methods

---
+++ String Methods/js.js
@@ -0,0 +1,31 @@
+// charAt 
+/*var country= "Bangladesh"
+
+document.write(
+    country.charAt(0)
+)
+
+
+// concat
+var country1="Bangladesh"
+var country2="India"
+var country3="Pakistan"
+
+document.write(
+    country1.concat(country3)
+)
+
+// indexof
+var country="bangladesh is a small country"
+
+document.write(
+    country.indexOf("t")
+)
+
+
+// Last indexof
+
+var country="India"
+document.write(
+    country.lastIndexOf("a")
+)*/
