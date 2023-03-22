b32ef154d99b634c3930eaad216dc3ab7be3e14a
Developertaposroy <taposroy616@gmail.com>
2023-03-22 13:34:24 +0600

    splice

---
+++ Array splice/js.js
@@ -0,0 +1,10 @@
+// remove
+// var numArray=[10,20,30,50,40,50,60,80,90,70,20,10];
+// numArray.splice(0,5)
+// document.write(numArray)
+
+// add 
+var numArray=[10,20,30,40,50,60,70,80,90]
+
+numArray.splice(5,0,100);
+document.write(numArray)
