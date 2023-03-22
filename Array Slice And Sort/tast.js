0013b1a29e51ee76fb471abc3b53675870b5520f
Developertaposroy <taposroy616@gmail.com>
2023-03-22 13:13:28 +0600

    Array Slice And Sort

---
+++ Array Slice And Sort/tast.js
@@ -0,0 +1,9 @@
+// slice
+/* var numArray=[10,50,60,40,20,30,80];
+var results= numArray.slice(3,5);
+document.write(results)*/
+ 
+//sort & reverse
+var numArray=[10,20,50,30,50,60,40,80,90]
+ numArray.sort().reverse()
+ document.write(numArray)
--- java/tast.js
+++
@@ -1,3 +0,0 @@
-var numArray=[10,50,60,40,20,30,80];
-var results= numArray.slice(3,5);
-document.write(results)
